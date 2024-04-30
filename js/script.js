5e110a4ea43c3e623362dd495116d46d6670e44e
DanielJooste <160589501+DanielJooste@users.noreply.github.com>
2024-04-30 12:53:14 +0200

    console log

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log ("hello world");
+console.log ("I am " + 77+ "years old");
+console.log (87+32 );
+
+let ticketNum = 3;
+const pi =3.14;
+
+let firstName = "Tayla", surname= "Bekker", faveNum = 77;
